e4710a6eb16554b2ee07df49cb2ea7807bcc472b
qlemql <taehyun_fe@naver.com>
2021-10-05 13:32:49 +0900

    React State
    state를 왜 사용하는가? 변수가 변경될 때 자동으로 HTML을 재렌더링되게 만들고 싶어서, 기존 변수로 만들때는 변수가 바뀌면 직접 홈페이지를 새로고침해야 바뀐 변수가 반영되지만 state를 사용할 경우 홈페이지를 새로고침하지 않아도 데이터가 변하면 자동으로 재렌더링된다.
    
    state 사용방법 React의 내장함수 {useState}를 불러온 뒤 let [변수명, 바뀔변수명] = useState("저장할 데이터")으로 작성 useState는 ES6 destructuring문법이 반영됨, useState() ()안에는 Array, Object 자료형 다 들어갈 수 있다. 변수처럼 사용할 곳에 {변수명}중괄호 이용

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,32 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  let data = "changeData";
+  let [글제목, 글제목변경] = useState("React");
+
+  let posts = "데이터 맛집";
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-      <div style={{ color: "blue", fontSize: "30px" }}>{data}</div>
+      <div className="list">
+        <h3> {글제목} </h3>
+        <p>10월 5일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {posts} </h3>
+        <p>10월 5일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {posts} </h3>
+        <p>10월 5일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
