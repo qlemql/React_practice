0284b76fbdc0a2bc5a614905bde1c7917730f5c7
qlemql <taehyun_fe@naver.com>
2021-10-05 13:55:27 +0900

    React State
    useState로 값 변경해보기 : like의 값을 클릭할 때 마다 변경시켜주려면 setLike(대체할 데이터)를 이용한다. => setLike(like + 1) 소괄호()내에 있는 데이터로 완전히 대체해줌

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,27 +3,43 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  let [글제목, 글제목변경] = useState("React");
-
-  let posts = "데이터 맛집";
+  let [글제목, 글제목변경] = useState(["React", "Javascript", "Etc"]);
+  let [like, setLike] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
+      <button
+        onClick={() => {
+          글제목변경(["Angular", "Javascript", "Etc"]);
+        }}
+      >
+        버튼
+      </button>
       <div className="list">
-        <h3> {글제목} </h3>
+        <h3>
+          {글제목[0]}
+          <span
+            onClick={() => {
+              setLike(like + 1);
+            }}
+          >
+            👍
+          </span>
+          {like}
+        </h3>
         <p>10월 5일 발행</p>
         <hr />
       </div>
       <div className="list">
-        <h3> {posts} </h3>
+        <h3> {글제목[1]} </h3>
         <p>10월 5일 발행</p>
         <hr />
       </div>
       <div className="list">
-        <h3> {posts} </h3>
+        <h3> {글제목[2]} </h3>
         <p>10월 5일 발행</p>
         <hr />
       </div>
