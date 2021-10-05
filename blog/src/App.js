0fdabc975086e8a30c720037b56b39653cfb2141
qlemql <taehyun_fe@naver.com>
2021-10-05 13:15:49 +0900

    JSX 문법 1. class대신 className사용
    JSX 문법 2. 변수명, 함수명을 href, id, className, src등 여러가지 HTML 속성에 {}로 바인딩가능
    
    JSX 문법 3. inline style은 style={}안에 오브젝트 형으로 삽입 stlye={{color : red}}

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let data = "changeData";
+
   return (
     <div className="App">
-      
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <div style={{ color: "blue", fontSize: "30px" }}>{data}</div>
     </div>
   );
 }
