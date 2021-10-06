198799288b30bbd525a446e32939c26c7604e6c7
qlemql <taehyun_fe@naver.com>
2021-10-06 17:56:25 +0900

    React Input값 처리
    input값을 받고싶다면 onChange함수를 사용해서 받을 수 있다. onChange={(e)=>{e.target.value}} input값을 e.target.value로 받아 state에 값을 저장시킬 수 있다. 추후에 저장한 state값을 서버에 전달하는등 다양한 방법으로 사용 가능하다.

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,6 +6,10 @@ function App() {
   let [글제목, 글제목변경] = useState(["React", "Javascript", "Etc"]);
   let [like, setLike] = useState(0);
 
+  let [modal, modal변경] = useState(false);
+
+  let [입력값, 입력값변경] = useState("");
+
   function 제목바꾸기() {
     let newArray = [...글제목];
     newArray[0] = "Angular";
@@ -17,10 +21,22 @@ function App() {
     글제목변경(새로운정렬);
   }
 
-  let [modal, modal변경] = useState(false);
-
   function 모달() {
     modal변경(!modal);
+  }
+
+  function 입력값저장(e) {
+    입력값변경(e.target.value);
+  }
+
+  function 글내용(e) {
+    입력값변경(e.target.value);
+  }
+
+  function 글저장() {
+    let newArrays = [...글제목];
+    newArrays.unshift(입력값);
+    글제목변경(newArrays);
   }
 
   return (
@@ -30,9 +46,9 @@ function App() {
       </div>
       <button onClick={제목바꾸기}>버튼</button>
       <button onClick={정렬하기}>정렬</button>
-      {글제목.map((글) => {
+      {글제목.map((글, i) => {
         return (
-          <div className="list">
+          <div className="list" key={i}>
             <h3>
               {글}
               <span
@@ -49,6 +65,13 @@ function App() {
           </div>
         );
       })}
+
+      <div className="publish">
+        <input onChange={글내용} />
+        <button onClick={글저장}>저장</button>
+      </div>
+
+      <input onChange={입력값저장} />
 
       <button onClick={모달}>모달 버튼</button>
 
