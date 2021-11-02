29c67dc0585c13aa78a2e3a17c43cbd37e6c95a2
qlemql <taehyun_fe@naver.com>
2021-11-02 13:56:52 +0900

    setting router

--- prac_table/src/App.js
+++ prac_table/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router,Route, Switch, Redirect } from 'react-router-dom';
+import Detail from './Pages/Detail';
+import Main from './Pages/Main';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    <Router>
+      <Switch>
+        <Redirect from="/" to="Main" exact />
+        <Route path="/main" component={Main} exact />
+        <Route path="Detail" component={Detail} exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
---
+++ prac_table/src/Pages/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from '../Components/Nav';
+
+const Main = () => {
+    return (
+        <>
+        <Nav />
+        <div>Main Page</div>
+        </>
+    )
+}
+
+export default Main;
