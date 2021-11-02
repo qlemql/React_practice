69482d07651fd6676206f23732323a144149f8a0
qlemql <taehyun_fe@naver.com>
2021-11-02 19:47:50 +0900

    ''

--- prac_table/src/Pages/Detail.js
+++ prac_table/src/Pages/Detail.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
+import MaterialTable from 'material-table'
 
 const Detail = () => {
+    const [tableData, setTableData] = useState([]);
+    const columns = [
+        {title:"Name", field:"name"},
+        {title:"Email", field:"email"},
+        {title:"Photo", field:"photo"},
+    ]
     return (
         <>
         <div>Detail Page</div>
+        <MaterialTable columns={columns} data={tableData} />
         </>
     )
 }
